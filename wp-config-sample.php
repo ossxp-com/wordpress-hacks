744a33400ac376d97b557c9b526c5481c73e1eb8
Jiang Xin <jiangxin@ossxp.com>
2009-12-30 10:34:31 +0800

    Fixed #94, #99: new wp-config template, set default language and default FS_METHOD.

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -64,7 +64,12 @@ $table_prefix  = 'wp_';
  * de.mo to wp-content/languages and set WPLANG to 'de' to enable German
  * language support.
  */
-define ('WPLANG', '');
+define ('WPLANG', 'zh_CN');
+
+/**
+ * Direct download plugins to server
+ */
+define ('FS_METHOD', 'direct');
 
 /* That's all, stop editing! Happy blogging. */
 
