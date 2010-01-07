8841577a0778db8ac981e717148ee3460f3ad034
Jiang Xin <jiangxin@ossxp.com>
2010-01-07 18:54:54 +0800

    %link should not be translated.

--- wp-content/themes/default/single.php
+++ wp-content/themes/default/single.php
@@ -12,8 +12,8 @@ get_header();
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<div class="navigation">
-			<div class="alignleft"><?php previous_post_link(__('&laquo; %link')) ?></div>
-			<div class="alignright"><?php next_post_link(__('%link &raquo;')) ?></div>
+			<div class="alignleft"><?php previous_post_link('&laquo; %link') ?></div>
+			<div class="alignright"><?php next_post_link('%link &raquo;') ?></div>
 		</div>
 
 		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
